36a03f3d38027954f9c5f30a9470f597536f1c76
Lucas Noan <noanpires@gmail.com>
2024-04-03 14:39:13 -0300

    0

---
+++ aula04.cpp
@@ -0,0 +1,20 @@
+// Regras para nomes de Classes, Métodos e Variáveis
+//
+// -Pode conter Letras e Números.
+// -Uma Variável pode começar com qualquer Letra mas nunca com um número.
+// -O nome não pode conter espaço. EX: minha Classe
+// -O único caracteres especial que pode ser usado é o UnderLine.
+// -Quando o nome da variável/classe for composto, colocar a primeira letra da primeira palavra Minúscula e a primeira letra da segunda palavra Maiúscula. EX: minhaClasse / classe1 / varTempoTot
+// -A estrutura de declaração sempre vai ser: tipo nome = valor
+#include <iostream>
+#include <stdlib.h>
+#include <locale.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"Portuguese_Brazil");
+
+    system("pause");
+    return 0;
+}
---
+++ aula05.cpp
@@ -0,0 +1,16 @@
+//Introdução ás Stream (Stream é uma sequência/fluxo de dados)
+// - Tipos de Stream: Stream de Texto, constituído por caracteres ASCII (texto). Stream Binária, bytes "puros".
+//
+#include <iostream>
+#include <stdlib.h>
+#include <locale.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"Portuguese_Brazil");
+
+
+    system("pause");
+    return 0;
+}
